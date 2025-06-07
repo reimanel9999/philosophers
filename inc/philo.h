3cc7b94018c38433d3ab9eaf45c671e4db023bba
reimanel9999 <manocassc@gmail.com>
2025-06-07 13:44:31 +0000

    teste!

---
+++ inc/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manu <manu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/07 13:39:02 by manu              #+#    #+#             */
+/*   Updated: 2025/06/07 13:43:42 by manu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <string.h>
+# include <errno.h>
+# include <fcntl.h>
